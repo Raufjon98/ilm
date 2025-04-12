9348d178bdf73ff57f8b2c22407255f34794aeb1
raufdzhon_aliboev <raufjonaliboev77@gmail.com>
2025-04-12 22:45:55 +0900

    Changed prameter of Methods GetStudentById and GetTecherById from int to string!

--- ilmV3/src/Application/Student/Queries/GetStudent.cs
+++ ilmV3/src/Application/Student/Queries/GetStudent.cs
@@ -1,26 +1,29 @@
-﻿using ilmV3.Application.Common.Security;
+﻿using ilmV3.Application.Common.Interfaces;
+using ilmV3.Application.Common.Security;
 using ilmV3.Domain.Constants;
 using ilmV3.Domain.interfaces;
 
 namespace ilmV3.Application.Student.Queries;
 
 [Authorize(Policy = Policies.CanRead)]
-public record GetStudentQuery(int studentId) : IRequest<StudentVM>;
+public record GetStudentQuery(string studentId) : IRequest<StudentVM>;
 
 public class GetStudentQueryHandler : IRequestHandler<GetStudentQuery, StudentVM>
 {
     private readonly IStudentRepository _studentRepository;
-    public GetStudentQueryHandler(IStudentRepository studentRepository, IMapper mapper)
+    private readonly IIdentityService _identityService;
+    public GetStudentQueryHandler(IStudentRepository studentRepository, IIdentityService identityService)
     {
+        _identityService = identityService;
         _studentRepository = studentRepository;
     }
     public async Task<StudentVM> Handle(GetStudentQuery request, CancellationToken cancellationToken)
     {
-        var student = await _studentRepository.GetStudentByIdAsync(request.studentId);
-        if (student == null)
-        {
-            throw new KeyNotFoundException($"Record with ID {request.studentId} not found.");
-        }
+        var user = await _identityService.GetUserByIdAsync(request.studentId);
+        ArgumentNullException.ThrowIfNull(user);
+
+        var student = await _studentRepository.GetStudentByIdAsync(user.ExternalUserId);
+        ArgumentNullException.ThrowIfNull(student);
         var studentVM = new StudentVM()
         {
             Id = student.Id,
